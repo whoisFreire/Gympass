2bdb4d70128d4cd02818ecaa3458827791713ef8
Leonardo Freire <leo.freirerussi@outlook.com>
2023-10-07 17:20:15 -0300

    criando caso de uso de perfil do usuario

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,6 +3,15 @@ import { Prisma, User } from '@prisma/client'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
+
+  async findById(id: string) {
+    const user = this.items.find((user) => user.id === id)
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = this.items.find((user) => user.email === email)
     if (!user) {
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { describe, it, expect, beforeEach } from 'vitest'
+import { hash } from 'bcryptjs'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('Should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      id: 'user-1',
+      name: 'test',
+      email: 'test@test.com',
+      password_hash: await hash('123456', 6),
+      created_at: new Date(),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toEqual('test')
+  })
+
+  it('Should not be able to get user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'non-user-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
